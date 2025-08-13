fb73eea512baae6b9f6716dae7314f05980232cf
Geoff Leyland <geoff.leyland@incremental.co.nz>
2025-08-13 21:35:24 +1200

    Show the git hash in the version string.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use git_version::git_version;
+
+
 //-------------------------------------------------------------------------------------------------
 
-fn main() {
+fn write_version() {
     let name: &str = env!("CARGO_PKG_NAME");
     let version: &str = env!("CARGO_PKG_VERSION");
-    eprintln!("{name} {version}");
+    let git_version: &str = git_version!();
+    eprintln!("{name} {version} ({git_version})");
+}
+
+fn main() {
+    write_version();
 }
 
 
