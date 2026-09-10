c2098bdc624c2b5a9bef64b302696cad082b9fa1
Geoff Leyland <geoff.leyland@incremental.co.nz>
2026-09-10 23:58:05 +1200

    Assembler.rs's assemble! macro *always had None as a span.

--- src/codegen/assembler.rs
+++ src/codegen/assembler.rs
@@ -15,10 +15,10 @@ macro_rules! asm_op {
 }
 
 macro_rules! assemble {
-    ($vec:expr, $span:expr, $op:ident $(, $($operand:tt $operand_arg:expr),*)?) => {
+    ($vec:expr, $op:ident $(, $($operand:tt $operand_arg:expr),*)?) => {
         $vec.push(Instr {
             code: &isa::$op,
-            span: $span,
+            span: None,
             operands: vec![$($(asm_op!($operand($operand_arg))),*)?],
         })
     }
@@ -79,8 +79,8 @@ fn emit_function(
     // Save any callee saved registers
     for pair in regs_to_save.chunks(2) {
         match *pair {
-            [a, b]  => assemble!(instrs, None, stp_d_pre, Reg(a), Reg(b), Reg(REGS.stack_reg), Offset(-16)),
-            [a]     => assemble!(instrs, None, str_d_pre, Reg(a), Reg(REGS.stack_reg), Offset(-16)),
+            [a, b]  => assemble!(instrs, stp_d_pre, Reg(a), Reg(b), Reg(REGS.stack_reg), Offset(-16)),
+            [a]     => assemble!(instrs, str_d_pre, Reg(a), Reg(REGS.stack_reg), Offset(-16)),
             _       => unreachable!()
         }
     }
@@ -107,21 +107,21 @@ fn emit_function(
         if ai.code.restore_regs() {
             for pair in regs_to_save.chunks(2).rev() {
                 match *pair {
-                    [a, b]  => assemble!(instrs, None, ldp_d_post, Reg(a), Reg(b), Reg(REGS.stack_reg), Offset(16)),
-                    [a]     => assemble!(instrs, None, ldr_d_post, Reg(a), Reg(REGS.stack_reg), Offset(16)),
+                    [a, b]  => assemble!(instrs, ldp_d_post, Reg(a), Reg(b), Reg(REGS.stack_reg), Offset(16)),
+                    [a]     => assemble!(instrs, ldr_d_post, Reg(a), Reg(REGS.stack_reg), Offset(16)),
                     _       => unreachable!()
                 }
             }
         }
 
         if ai.code.save_link_reg() {
-            assemble!(instrs, None, str_x_pre, Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(-16));
+            assemble!(instrs, str_x_pre, Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(-16));
         }
 
         instrs.push(Instr{ code: ai.code, operands, span: Some(ai.span) });
 
         if ai.code.save_link_reg() {
-            assemble!(instrs, None, ldr_x_post, Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(16));
+            assemble!(instrs, ldr_x_post, Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(16));
         }
     }
 }
@@ -166,17 +166,17 @@ fn move_regs(moves: &[(MachineReg, MachineReg)], temp_reg: MachineReg, instrs: &
         let Some(copy) = copies[usize::from(source)] {
             sources[usize::from(*destination)] = None;
             move_regs_backwards(source, &mut sources, &mut destination_counts, instrs);
-            assemble!(instrs, None, fmov_d, Reg(*destination), Reg(copy));
+            assemble!(instrs, fmov_d, Reg(*destination), Reg(copy));
         }
     }
 
     // Now do the ones where there's no other copy and we need a temp.
     for (_, destination) in moves {
         let Some(source) = sources[usize::from(*destination)] else { continue };
-        assemble!(instrs, None, fmov_d, Reg(temp_reg), Reg(source));
+        assemble!(instrs, fmov_d, Reg(temp_reg), Reg(source));
         sources[usize::from(*destination)] = None;
         move_regs_backwards(source, &mut sources, &mut destination_counts, instrs);
-        assemble!(instrs, None, fmov_d, Reg(*destination), Reg(temp_reg));
+        assemble!(instrs, fmov_d, Reg(*destination), Reg(temp_reg));
     }
 }
 
@@ -184,7 +184,7 @@ fn move_regs(moves: &[(MachineReg, MachineReg)], temp_reg: MachineReg, instrs: &
 fn move_regs_backwards(mut destination: MachineReg, sources: &mut[Option<MachineReg>], destination_counts: &mut[u8], instrs: &mut Vec<Instr>) {
     loop {
         let Some(source) = sources[usize::from(destination)] else { return };
-        assemble!(instrs, None, fmov_d, Reg(destination), Reg(source));
+        assemble!(instrs, fmov_d, Reg(destination), Reg(source));
         sources[usize::from(destination)] = None;
         destination_counts[usize::from(source)] -= 1;
         if destination_counts[usize::from(source)] > 0 { return }
@@ -200,30 +200,30 @@ fn emit_glue(argument_count: u8, return_count: u8, assembler: &mut Vec<Instr>) {
     // In fact, at the moment, we only have floating-point arguments, so it *won't* get clobbered,
     // but if I ever get to types and integers, then I don't want to have a mystery bug strike me
     // because I was too smart about my function glue.
-    assemble!(assembler, None, mov_x, Reg(REGS.scratch_reg), RawReg(0));
+    assemble!(assembler, mov_x, Reg(REGS.scratch_reg), RawReg(0));
 
     // Move the output buffer and the return address to the stack, since they're about to get
     // overwritten and we need them later.
-    assemble!(assembler, None, stp_x_pre, RawReg(1), Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(-16));
+    assemble!(assembler, stp_x_pre, RawReg(1), Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(-16));
 
     // Move the arguments from the input buffer into the argument registers.
     for i in 0..argument_count {
-        assemble!(assembler, None, ldr_d_offset, RawReg(i), Reg(REGS.scratch_reg), Offset(i32::from(i) * 8));
+        assemble!(assembler, ldr_d_offset, RawReg(i), Reg(REGS.scratch_reg), Offset(i32::from(i) * 8));
     }
 
     // Call our function
-    assemble!(assembler, None, bl,
+    assemble!(assembler, bl,
         Offset(-4 * i32::try_from(assembler.len())
             .expect("internal compiler error: function too long for jump")));
 
     // Load the output buffer in to x16 and the return address to the appropriate spot
-    assemble!(assembler, None, ldp_x_post, Reg(REGS.scratch_reg), Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(16));
+    assemble!(assembler, ldp_x_post, Reg(REGS.scratch_reg), Reg(REGS.link_reg), Reg(REGS.stack_reg), Offset(16));
 
     for i in 0..return_count {
-        assemble!(assembler, None, str_d_offset, RawReg(i), Reg(REGS.scratch_reg), Offset(i32::from(i) * 8));
+        assemble!(assembler, str_d_offset, RawReg(i), Reg(REGS.scratch_reg), Offset(i32::from(i) * 8));
     }
 
-    assemble!(assembler, None, ret);
+    assemble!(assembler, ret);
 }
 
 
