42aa221a1442b872da92165966e04001a91b8a13
Geoff Leyland <geoff.leyland@incremental.co.nz>
2026-09-13 20:44:12 +1200

    Compute the complete set of slot moves in allocate, instead of doing the last bit in lower_to_regs

--- src/codegen/allocator.rs
+++ src/codegen/allocator.rs
@@ -158,6 +158,7 @@ fn lower_to_slots_and_split(
 
 struct SlotResult {
     reg:                                Option<MachineReg>,
+    moves:                              Vec<(MachineReg, MachineReg)>,
     temp_reg:                           MachineReg,
 }
 
@@ -233,6 +234,20 @@ fn allocate(
         }
     }
 
+
+    // Unify fixed_input moves and slot_moves and figure out which slot moves are actually between
+    // registers.
+    let mut moves = vec![vec![]; slot_count];
+    for instr in instrs {
+        moves[instr.slot] = instr.fixed_inputs.iter()
+            .map(|(slot, reg)| (regs[*slot].get().copied().unwrap(), *reg))
+            .chain(instr.slot_moves.iter()
+                .map(|(src, dst)|
+                    (regs[*src].get().copied().unwrap(), regs[*dst].get().copied().unwrap())))
+            .filter(|(source, dest)| source != dest)
+            .collect();
+    }
+
     // If an instruction needs a temporary register (for swaps *before* the instruction), the
     // registers available for a temp are the registers available for the instruction MINUS
     // the arguments to the instruction (which, if this is the last use of the argument are
@@ -248,8 +263,8 @@ fn allocate(
         .map(|&a| REGS.best_reg(D_BANK, a, None))
         .collect::<Vec<_>>();
 
-    regs.iter_mut().zip(temp_regs)
-        .map(|(reg, temp_reg)| SlotResult { reg: reg.take(), temp_reg })
+    regs.iter_mut().zip(temp_regs).zip(moves)
+        .map(|((reg, temp_reg), moves)| SlotResult { reg: reg.take(), moves, temp_reg })
         .collect()
 }
 
@@ -294,8 +309,7 @@ pub(super) struct Instr {
 
 fn lower_to_regs(
     instrs:                             &[SlotInstr],
-
-    slot_allocation:                    &[SlotResult],
+    slot_allocations:                   &[SlotResult],
 ) -> Vec<Instr> {
     instrs.iter().map(|instr| {
         let mut operands = vec![];
@@ -304,28 +318,19 @@ fn lower_to_regs(
                 SlotOperand::Constant(i)    => operands.push(Operand::Constant(*i)),
                 SlotOperand::Function(name) => operands.push(Operand::Function(name.clone())),
                 SlotOperand::Slot(s)        => {
-                    operands.push(Operand::Reg(slot_allocation[*s].reg
+                    operands.push(Operand::Reg(slot_allocations[*s].reg
                         .expect("internal compiler error: no register assigned for slot")));
                 }
             }
         }
-        let mut moves = vec![];
-        for (slot, required_reg) in &instr.fixed_inputs {
-            let slot_reg = slot_allocation[*slot].reg
-                .expect("internal compiler error: no register assigned for slot");
-            if slot_reg != *required_reg { moves.push((slot_reg, *required_reg)); }
-        }
-        for (source, dest) in &instr.slot_moves {
-            let source_reg = slot_allocation[*source].reg.expect("internal compiler error: no register assigned for slot");
-            let dest_reg = slot_allocation[*dest].reg.expect("internal compiler error: no register assigned for slot");
-            if source_reg != dest_reg { moves.push((source_reg, dest_reg)); }
-        }
 
+        let allocation = &slot_allocations[instr.slot];
         Instr{
-            operands, moves,
+            operands,
             code:                       instr.code,
-            result_reg:                 slot_allocation[instr.slot].reg,
-            temp_reg:                   slot_allocation[instr.slot].temp_reg,
+            result_reg:                 allocation.reg,
+            moves:                      allocation.moves.clone(),
+            temp_reg:                   allocation.temp_reg,
 
             #[cfg(feature = "dogfood")]
             span:                       instr.span,
